24ec82974fd7ed89488eed6de4d2cebc34535001
Ярослав <yaroslavsevryukov77@gmail.com>
2023-12-18 19:34:25 +0300

    add one.py

---
+++ 2023.12.08/one.py
@@ -0,0 +1,14 @@
+import model
+import view
+import controller
+
+
+def main():
+    m = model.FileIO()
+    v = view.CLI()
+    c = controller.Application(m, v)
+
+    c.loop()
+
+if __name__ == "__main__":
+    main()
